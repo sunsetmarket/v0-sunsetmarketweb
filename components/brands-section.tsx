d1243a55174449363fadbce901f4a374b7efc810
sunsetmarket <sunsetmarketmarbella@gmail.com>
2026-03-24 17:41:01 -0400

    Update brands-section.tsx

--- components/brands-section.tsx
+++ components/brands-section.tsx
@@ -81,9 +81,8 @@ export function BrandsSection() {
             </div>
 
             <Button 
-              onClick={() => setShowForm(!showForm)}
-              className="bg-primary hover:bg-primary/90 hover:scale-105 text-primary-foreground px-10 py-7 text-base font-medium tracking-wide rounded-xl transition-all duration-300 shadow-lg hover:shadow-xl"
-            >
+               onClick={() => window.open("https://sunsetmarketmarbellaincripcion.fillout.com/t/h7AcuZDogqus", "_blank")}
+  className="bg-primary hover:bg-primary/90 hover:scale-105 text-primary-foreground px-10 py-7 text-base font-medium tracking-wide rounded-xl transition-all duration-300 shadow-lg hover:shadow-xl"
               APLICAR COMO EXPOSITOR
             </Button>
 
